846c6f612a6185f101c22d7cc718192c985c0ca6
Vladislav <vladislav.yashin@yahoo.com>
2016-12-05 08:12:39 +0500

    license attribution

--- src/Domain.Infrastructure/Fetching/IFetchRequest.cs
+++ src/Domain.Infrastructure/Fetching/IFetchRequest.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Credit to http://stackoverflow.com/users/718670/guido and http://stackoverflow.com/users/647901/seldon
+// http://stackoverflow.com/a/5742158
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
